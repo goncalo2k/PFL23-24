d5679608ad4c3ab44207c4d2281a1f8d9f77a937
PedroPF1234 <pefonseca10@gmail.com>
2023-12-29 15:12:59 +0000

    Fixed wrong variable calls and added most work from part 2

--- Projeto 2/TP2-quickstart.hs
+++ Projeto 2/TP2-quickstart.hs
@@ -3,6 +3,7 @@
 
 -- Part 1
 import Data.List
+import Data.Char
 
 data Inst =
   Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | Store String | Noop |
@@ -21,15 +22,6 @@ createEmptyStack = []
 
 createEmptyState :: State
 createEmptyState = []
-
-
-addIntToStr :: Val -> String -> String
-addIntToStr n s = if s == "" then boolValueToString n else s ++ "," ++ boolValueToString n
-  where
-    boolValueToString v = case v of
-                          TT -> "True"
-                          FF -> "False"
-                          _ -> show v
 
 stack2Str :: Stack -> String
 stack2Str = intercalate "," . map valToString
@@ -135,22 +127,79 @@ main = do
 
 -- TODO: Define the types Aexp, Bexp, Stm and Program
 
--- compA :: Aexp -> Code
+data Expr = AExp Aexp Aexp | BExp Bexp Bexp
+    deriving (Show, Eq)
+
+data Aexp = Const Integer
+          | Var String
+          | ADD Aexp Aexp
+          | SUB Aexp Aexp
+          | MUL Aexp Aexp
+          deriving (Show, Eq)
+
+data Bexp = BoolConst Bool
+          | AND Bexp Bexp
+          | OR Bexp Bexp
+          | NOT Bexp
+          | EQ Expr
+          deriving (Show, Eq)
+
+data Stm = ASS String Aexp
+         | IF Bexp Stm Stm
+         | WHI Bexp Stm
+         | SEQ [Stm]
+         deriving (Show, Eq)
+
+
+type Program = [Stm]
+
+compA :: Aexp -> Code
 compA = undefined -- TODO
 
--- compB :: Bexp -> Code
+compB :: Bexp -> Code
 compB = undefined -- TODO
 
--- compile :: Program -> Code
+compile :: Program -> Code
 compile = undefined -- TODO
 
--- parse :: String -> Program
-parse = undefined -- TODO
+lexer :: String -> [String]
+lexer [] = []
+lexer (c:cs)
+    | isSpace c = lexer cs
+    | c `elem` ";(),*-+" = [c] : lexer cs
+    | c == '=' = if not (null cs) && head cs == '='
+                 then "==" : lexer (tail cs)
+                 else "=" : lexer cs
+    | c == ':' = if not (null cs) && head cs == '='
+                 then ":=" : lexer (tail cs)
+                 else ":" : lexer cs
+    | otherwise = let (token, rest) = span isTokenChar (c:cs)
+                  in token : lexer rest
+  where
+    isTokenChar x = not (isSpace x || x `elem` ";(),*=-")
+
+
+-- Parses an arithmetic expression and returns the expression and the remaining tokens
+parseAexp :: [String] -> (Aexp, [String])
+parseAexp tokens = undefined
+
+-- Parses a boolean expression and returns the expression and the remaining tokens
+parseBexp :: [String] -> (Bexp, [String])
+parseBexp tokens = undefined
+
+-- Parses a statement and returns the statement and the remaining tokens
+parseStm :: [String] -> (Stm, [String])
+parseStm tokens = undefined
+
+
+parse :: String -> Program
+parse "" = []
+parse s = undefined 
 
 -- To help you test your parser
 testParser :: String -> (String, String)
-testParser programCode = (stack2Str stack, store2Str store)
-  where (_,stack,store) = run (compile (parse programCode), createEmptyStack, createEmptyStore)
+testParser programCode = (stack2Str stack, state2Str state)
+  where (_,stack,state) = run (compile (parse programCode), createEmptyStack, createEmptyState)
 
 -- Examples:
 -- testParser "x := 5; x := x - 1;" == ("","x=4")
@@ -160,3 +209,33 @@ testParser programCode = (stack2Str stack, store2Str store)
 -- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
 -- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
 -- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+
+
+{- Main para testar a funcionalidade so far
+main :: IO ()
+main = do
+   let result1 = boolToString (testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10",""))
+       result2 = boolToString (testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True"))
+       result3 = boolToString (testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False"))
+       result4 = boolToString (testAssembler [Push (-20),Tru,Fals] == ("False,True,-20",""))
+       result5 = boolToString (testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20",""))
+       result6 = boolToString (testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20",""))
+       result7 = boolToString (testAssembler [Push (-20),Push (-21), Le] == ("True",""))
+       result8 = boolToString (testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4"))
+       result9 = boolToString (testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1"))
+
+       result10 = tupleToString(testAssembler [Push 1,Push 2,And])
+       result11 = tupleToString(testAssembler [Tru,Tru,Store "y", Fetch "x",Tru])
+
+   putStr ("result1: " ++ result1 ++ "\n")
+   putStr ("result2: " ++ result2 ++ "\n")
+   putStr ("result3: " ++ result3 ++ "\n")
+   putStr ("result4: " ++ result4 ++ "\n")
+   putStr ("result5: " ++ result5 ++ "\n")
+   putStr ("result6: " ++ result6 ++ "\n")
+   putStr ("result7: " ++ result7 ++ "\n")
+   putStr ("result8: " ++ result8 ++ "\n")
+   putStr ("result9: " ++ result9 ++ "\n")
+   putStr ("result10: " ++ result10 ++ "\n")
+   putStr ("result11: " ++ result11 ++ "\n")
+-}
